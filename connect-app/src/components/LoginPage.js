c127c1f02bef0b6a02c447f1d4444b5d9f599c40
juanaguirre30 <juanaguirre30@csu.fullerton.edu>
2025-01-29 18:42:24 -0800

    rough draft of login page

---
+++ connect-app/src/components/LoginPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './LoginPage.css';
+
+function LoginPage() {
+  return (
+    <div className="container">
+      <div className="top-right-title"> Connect The Memories </div>  
+      <div className="login-box">
+        <h1>Welcome!</h1>
+        <button className="btn">Log In</button>
+        <button className="btn">Create an Account</button>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
