2749a4502b4eda28571c162b34b6fad7e54b4e94
juanaguirre30 <juanaguirre30@csu.fullerton.edu>
2025-01-30 10:55:26 -0800

    Added button at bottom right of login box

--- connect-app/src/components/LoginPage.js
+++ connect-app/src/components/LoginPage.js
@@ -9,6 +9,7 @@ function LoginPage() {
         <h1>Welcome!</h1>
         <button className="btn">Log In</button>
         <button className="btn">Create an Account</button>
+        <div className="helpbtn"> ? </div> 
       </div>
     </div>
   );
