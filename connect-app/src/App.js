abcd11ccbd5df9c4a0d610383628e42705ff8869
juanaguirre30 <juanaguirre30@csu.fullerton.edu>
2025-01-29 18:47:04 -0800

    rought draft of the loginpage

--- connect-app/src/App.js
+++ connect-app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginPage from "./components/LoginPage"; // Import the LoginPage component
+import LoginPage from "./components/LoginPage"; 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
