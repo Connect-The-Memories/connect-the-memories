f5b22d89b77990f13fffd6945a5fa5d79beb5558
Brenda Pham <blpham21@gmail.com>
2025-02-16 22:16:39 -0800

    Update primary and support homepages to new UI design

--- connect-app/src/components/PrimaryHomePage.js
+++ connect-app/src/components/PrimaryHomePage.js
@@ -17,30 +17,34 @@ function PrimaryHomePage() {
         <button className="logout-button" onClick={() => navigate("/")}>LOGOUT</button>
       </nav>
       <div className="inner-box">
+        <div className="welcome-message">Welcome, {userName}!</div>
         <div className="inner-box-top-row">
-          <button className="progress-button" onClick={() => navigate("/progress")}>View Progress</button>
+          <div style={{ width: "27vw" }}></div>
+          <button className="top-row-button" onClick={() => navigate("/stats")}>Exercise Statistics</button>
+          <button className="top-row-button" onClick={() => navigate("/journal")}>Journal</button>
         </div>
         <div className="action-buttons-container">
           <div className="button-container">
-            <button className=" action-button gallery-button" onClick={() => navigate("/gallery")}>
+            <button className=" action-button" onClick={() => navigate("/gallery")}>
               <img src={galleryIcon} alt="gallery icon" className="action-button-icon" />
+              <p className="action-buttons-text">Media Gallery</p>
             </button>
-            <p className="action-buttons-text">Media Gallery</p>
           </div>
           <div className="button-container">
-            <button className="action-button exercises-button" onClick={() => navigate("/exerciseselection")}>
-              <img src={exerciseIcon} alt="exercise-icon" className="action-button-icon" /></button>
-            <p className="action-buttons-text">Exercises</p>
+            <button className="action-button" onClick={() => navigate("/exerciseselection")}>
+              <img src={exerciseIcon} alt="exercise-icon" className="action-button-icon" />
+              <p className="action-buttons-text">Exercises</p>
+            </button>
           </div>
           <div className="button-container">
-            <button className="action-button add-button" onClick={() => navigate("/addsupport")}>
-              <img src={friendIcon} alt="friends icon" className="action-button-icon" /></button>
-            <p className="action-buttons-text">Add Friends or Family</p>
+            <button className="action-button" onClick={() => navigate("/addsupport")}>
+              <img src={friendIcon} alt="friends icon" className="action-button-icon" />
+              <p className="action-buttons-text">Add Friends or Family</p>
+            </button>
           </div>
         </div>
-        <div className="welcome-message">Welcome, {userName}</div>
       </div>
-    </div>
+    </div >
   );
 }
 
