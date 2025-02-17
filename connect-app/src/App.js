ecbbbee85ac72fcb71dc78b57bd80edc25cb336c
Brenda Pham <blpham21@gmail.com>
2025-02-16 20:55:09 -0800

    Remove old login page, update to landing page

--- connect-app/src/App.js
+++ connect-app/src/App.js
@@ -3,12 +3,11 @@ import LandingPage from "./components/LandingPage";
 import PrimaryHomePage from "./components/PrimaryHomePage";
 import SupportHomePage from "./components/SupportHomePage";
 import HelpPage from "./components/HelpPage";
-import CreateAccount from "./components/CreateAccount"; 
-import LoggingIn from "./components/LoggingIn";
-import Gallery from "./components/Gallery"; 
+import CreateAccount from "./components/CreateAccount";
+import Gallery from "./components/Gallery";
 import ExerciseSelection from "./components/ExerciseSelection";
 import SpeedProcessing from "./components/SpeedProcessing";
-import MemoryGame from "./components/MemoryGame"; 
+import MemoryGame from "./components/MemoryGame";
 import ColorMatch from "./components/ColorMatch";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -18,15 +17,14 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/primaryhomepage" element={<PrimaryHomePage />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/exerciseselection" element={<ExerciseSelection />} /> 
-            <Route path="/speed-processing" element={<SpeedProcessing />} />
-            <Route path="/memorygame" element={<MemoryGame />} /> 
-            <Route path="/colormatch" element={<ColorMatch />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/exerciseselection" element={<ExerciseSelection />} />
+        <Route path="/speed-processing" element={<SpeedProcessing />} />
+        <Route path="/memorygame" element={<MemoryGame />} />
+        <Route path="/colormatch" element={<ColorMatch />} />
         <Route path="/supporthomepage" element={<SupportHomePage />} />
         <Route path="/help" element={<HelpPage />} />
-        <Route path="/createAccount" element={<CreateAccount />} /> 
-        <Route path="/loggingIn" element={<LoggingIn />} /> 
+        <Route path="/createAccount" element={<CreateAccount />} />
       </Routes>
     </Router>
   );
