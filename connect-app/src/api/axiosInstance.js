27f99a919a0375395e242adf2a6a35e86642c822
Nathaniel Kamal <110137450+Dossr-NK@users.noreply.github.com>
2025-02-18 02:26:43 -0800

    Update axiosInstance.js

--- connect-app/src/api/axiosInstance.js
+++ connect-app/src/api/axiosInstance.js
@@ -5,6 +5,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000/api",
 });
 
-axiosInstance.defaults.withCredentials = true;
+// Temporarily remove withCredentials for development purposes.
+// axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
