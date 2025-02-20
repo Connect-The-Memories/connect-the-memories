a754063b81003e190b0a410fe3ba45fb22a5d061
Dossr-NK <nathaniel.kamal13@gmail.com>
2025-02-20 01:52:17 -0800

    Temporarily hard code baseURL to test hosted backend.

--- connect-app/src/api/axiosInstance.js
+++ connect-app/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL || "http://127.0.0.1:5000/api",
+  baseURL: "https://flask-backend-qgnd4kcnka-uc.a.run.app/api" || "http://127.0.0.1:5000/api",
 });
 
 axiosInstance.defaults.withCredentials = true;
