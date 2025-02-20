8ca66ee5f3ed73c96320960583ca2646624705ce
Dossr-NK <nathaniel.kamal13@gmail.com>
2025-02-20 01:16:01 -0800

    Enabled production level api usage.

--- connect-app/src/api/axiosInstance.js
+++ connect-app/src/api/axiosInstance.js
@@ -5,7 +5,6 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000/api",
 });
 
-// Temporarily remove withCredentials for development purposes.
-// axiosInstance.defaults.withCredentials = true;
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
