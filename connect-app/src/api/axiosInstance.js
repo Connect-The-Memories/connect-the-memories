3f6ff8892a20b89b79df78f1dfa73d60fdba28d4
Dossr-NK <nathaniel.kamal13@gmail.com>
2025-02-20 01:23:12 -0800

    Update env variable usage.

--- connect-app/src/api/axiosInstance.js
+++ connect-app/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000/api",
+  baseURL: process.env.API_BASE_URL || "http://127.0.0.1:5000/api",
 });
 
 axiosInstance.defaults.withCredentials = true;
