9605ad84816c2ab8f98147fa3498bbab54686c78
Brenda Pham <blpham21@gmail.com>
2025-03-13 04:33:00 -0700

    Update upload media page to support multi file/message upload, file previews, and adding file descriptions/dates

--- connect-app/src/mockUsers.js
+++ connect-app/src/mockUsers.js
@@ -1,20 +1,21 @@
 const mockUsers = [
-    { email: "mainuser1@gmail.com", password: "123456", type: "main", media: [] },
+  { email: "mainuser1@gmail.com", password: "123456", type: "main", media: [], linkedUsers: ["supportuser1@gmail.com"] },
 
-    {email: "mainuser2@gmail.com", password: "california", type: "main", media: [
+  {
+    email: "mainuser2@gmail.com", password: "california", type: "main", media: [
       { type: "photo", url: "https://via.placeholder.com/150", uploadedBy: "supportuser1@gmail.com" },
       { type: "photo", url: "https://via.placeholder.com/150", uploadedBy: "supportuser2@gmail.com" },
       { type: "message", text: "I hope you're doing well!", uploadedBy: "supportuser1@gmail.com", date: "2025-02-10" },
-      {type: "message", text: "We are so proud of you!", uploadedBy: "supportuser2@gmail.com", date: "2025-02-02"},
-      {type: "message", text: "Hi Grandpa", uploadedBy: "supportuser2@gmail.com", date: "2025-02-02"},
-      {type: "message", text: "W Gramps", uploadedBy: "supportuser2@gmail.com", date: "2025-01-22"},
-      {type: "message", text: "Hi, how are you?", uploadedBy: "supportuser2@gmail.com", date: "2025-01-20"},
-    ] },
+      { type: "message", text: "We are so proud of you!", uploadedBy: "supportuser2@gmail.com", date: "2025-02-02" },
+      { type: "message", text: "Hi Grandpa", uploadedBy: "supportuser2@gmail.com", date: "2025-02-02" },
+      { type: "message", text: "W Gramps", uploadedBy: "supportuser2@gmail.com", date: "2025-01-22" },
+      { type: "message", text: "Hi, how are you?", uploadedBy: "supportuser2@gmail.com", date: "2025-01-20" },
+    ], linkedUsers: ["supportuser1@gmail.com", "supportuser2@gmail.com"]
+  },
 
-    { email: "supportuser1@gmail.com", password: "calstate", type: "support" },
-    { email: "supportuser2@gmail.com", password: "csuf", type: "support" },
-    { email: "bothuser1@gmail.com", password: "fullerton", type: "both" },
-  ];
-  
-  export default mockUsers;
-  
+  { email: "supportuser1@gmail.com", password: "calstate", type: "support", linkedUsers: ["mainuser2@gmail.com"] },
+  { email: "supportuser2@gmail.com", password: "csuf", type: "support" },
+  { email: "bothuser1@gmail.com", password: "fullerton", type: "both" },
+];
+
+export default mockUsers;
