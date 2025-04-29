b19af7be10da27d51a546730007cb8a025692a81
Brenda Pham <blpham21@gmail.com>
2025-04-29 03:01:43 -0700

    implement exercise data graphs

--- connect-app/src/components/Progress.js
+++ connect-app/src/components/Progress.js
@@ -11,7 +11,10 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer
+  ResponsiveContainer,
+  Area,
+  AreaChart,
+  ReferenceLine
 } from 'recharts';
 
 export default function Progress() {
@@ -59,6 +62,80 @@ export default function Progress() {
     time: a.avg_reaction_time
   }));
 
+  // Mock data to use locally (array of 14 days)
+  // const data = [
+  //   {
+  //     date: "2025-04-27",
+  //     normalized_accuracy: 0.23,
+  //     normalized_rt: 0.7349693311852756
+  //   },
+  //   {
+  //     date: "2025-04-28",
+  //     normalized_accuracy: 0.6,
+  //     normalized_rt: 0.3590438763409098
+  //   },
+  //   {
+  //     date: "2025-04-29",
+  //     normalized_accuracy: 1,
+  //     normalized_rt: 0.2305938453098948
+  //   },
+  //   {
+  //     date: "2025-04-27",
+  //     normalized_accuracy: 0.23,
+  //     normalized_rt: 0.7349693311852756
+  //   },
+  //   {
+  //     date: "2025-04-28",
+  //     normalized_accuracy: 0.6,
+  //     normalized_rt: 0.3590438763409098
+  //   },
+  //   {
+  //     date: "2025-04-29",
+  //     normalized_accuracy: 1,
+  //     normalized_rt: 0.2305938453098948
+  //   },
+  //   {
+  //     date: "2025-04-27",
+  //     normalized_accuracy: 0.23,
+  //     normalized_rt: 0.7349693311852756
+  //   },
+  //   {
+  //     date: "2025-04-28",
+  //     normalized_accuracy: 0.6,
+  //     normalized_rt: 0.3590438763409098
+  //   },
+  //   {
+  //     date: "2025-04-29",
+  //     normalized_accuracy: 1,
+  //     normalized_rt: 0.2305938453098948
+  //   },
+  //   {
+  //     date: "2025-04-27",
+  //     normalized_accuracy: 0.23,
+  //     normalized_rt: 0.7349693311852756
+  //   },
+  //   {
+  //     date: "2025-04-28",
+  //     normalized_accuracy: 0.6,
+  //     normalized_rt: 0.3590438763409098
+  //   },
+  //   {
+  //     date: "2025-04-29",
+  //     normalized_accuracy: 1,
+  //     normalized_rt: 0.2305938453098948
+  //   },
+  //   {
+  //     date: "2025-04-28",
+  //     normalized_accuracy: 0.6,
+  //     normalized_rt: 0.3590438763409098
+  //   },
+  //   {
+  //     date: "2025-04-29",
+  //     normalized_accuracy: 1,
+  //     normalized_rt: 0.2305938453098948
+  //   }
+  // ]
+
   return (
     <div className="hp-container">
       <nav className="nav-bar">
@@ -69,6 +146,63 @@ export default function Progress() {
           ← Back
         </button>
       </nav>
+
+      <div className="inner-box">
+        {/* <p>Accuracy</p> */}
+        <ResponsiveContainer width="100%" height="50%">
+          <AreaChart
+            data={attempts}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="date"
+              tickFormatter={(dateStr) => {
+                const date = new Date(dateStr);
+                return `${date.getMonth() + 1}/${date.getDate()}`;
+              }}
+            />
+            <YAxis tickFormatter={(value) => `${(value * 100).toFixed(0)}%`} domain={[0, 1]} />
+            <Tooltip
+              formatter={(value, name) => [`${(value * 100).toFixed(2)}%`, name]}
+            />
+            <Legend />
+            <Area type="monotone" dataKey="normalized_accuracy" name="Accuracy (%)" stroke="#8884d8" fill="#8884d8" activeDot={{ r: 8 }} />
+          </AreaChart>
+        </ResponsiveContainer>
+
+        {/* <p>Reaction Time</p> */}
+        <ResponsiveContainer width="100%" height="50%">
+          <AreaChart
+            data={attempts}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="date"
+              tickFormatter={(dateStr) => {
+                const date = new Date(dateStr);
+                return `${date.getMonth() + 1}/${date.getDate()}`;
+              }}
+            />
+            <YAxis domain={[0, 1]} />
+            <Tooltip />
+            <Legend />
+            <ReferenceLine y={1} label="Max" stroke="red" strokeDasharray="3 3" />
+            <Area type="monotone" dataKey="normalized_rt" name="Reaction Time" stroke="#82ca9d" fill="#82ca9d" />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
