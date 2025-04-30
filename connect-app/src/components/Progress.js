11f2e82030f0b02b42a40ec6d48a20525caa4366
Brenda Pham <blpham21@gmail.com>
2025-04-30 02:27:05 -0700

    simple styling for progress graphs

--- connect-app/src/components/Progress.js
+++ connect-app/src/components/Progress.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DarkModeToggle from './DarkModeToggle';
+import './Progress.css';
 import { getExerciseAttempts } from '../api/database';
 import { useAuth } from '../context/AuthContext';
 import {
@@ -24,37 +25,37 @@ export default function Progress() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    if (!token) {
-      setError("You are not logged in.");
-      setTimeout(() => navigate("/"), 100);
-      return;
-    }
+  //   if (!token) {
+  //     setError("You are not logged in.");
+  //     setTimeout(() => navigate("/"), 100);
+  //     return;
+  //   }
 
-    async function fetchData() {
-      try {
-        const res = await getExerciseAttempts();
-        console.log('✅ Attempts response:', res);
-        const raw = res.data.exercise_data || [];
-        const data = raw.map(item => ({
-          ...item,
-          timestamp: new Date(item.timestamp)
-        }));
-        data.sort((a, b) => b.timestamp - a.timestamp);
-        setAttempts(data);
-      } catch (err) {
-        console.error('Error fetching attempts:', err.response || err);
-        setError('Failed to load progress data');
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchData();
-  }, [token]);
+  //   async function fetchData() {
+  //     try {
+  //       const res = await getExerciseAttempts();
+  //       console.log('✅ Attempts response:', res);
+  //       const raw = res.data.exercise_data || [];
+  //       const data = raw.map(item => ({
+  //         ...item,
+  //         timestamp: new Date(item.timestamp)
+  //       }));
+  //       data.sort((a, b) => b.timestamp - a.timestamp);
+  //       setAttempts(data);
+  //     } catch (err) {
+  //       console.error('Error fetching attempts:', err.response || err);
+  //       setError('Failed to load progress data');
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
+  //   fetchData();
+  // }, [token]);
 
-  if (loading) return <p>Loading progress...</p>;
-  if (error) return <p>{error}</p>;
+  // if (loading) return <p>Loading progress...</p>;
+  // if (error) return <p>{error}</p>;
 
   const chartData = attempts.map(a => ({
     date: a.timestamp.toLocaleDateString(),
@@ -63,78 +64,78 @@ export default function Progress() {
   }));
 
   // Mock data to use locally (array of 14 days)
-  // const data = [
-  //   {
-  //     date: "2025-04-10",
-  //     normalized_accuracy: 0.23,
-  //     normalized_rt: 0.7349693311852756
-  //   },
-  //   {
-  //     date: "2025-04-11",
-  //     normalized_accuracy: 0.6,
-  //     normalized_rt: 0.3590438763409098
-  //   },
-  //   {
-  //     date: "2025-04-12",
-  //     normalized_accuracy: 1,
-  //     normalized_rt: 0.2305938453098948
-  //   },
-  //   {
-  //     date: "2025-04-13",
-  //     normalized_accuracy: 0.23,
-  //     normalized_rt: 0.7349693311852756
-  //   },
-  //   {
-  //     date: "2025-04-14",
-  //     normalized_accuracy: 0.6,
-  //     normalized_rt: 0.3590438763409098
-  //   },
-  //   {
-  //     date: "2025-04-15",
-  //     normalized_accuracy: 1,
-  //     normalized_rt: 0.2305938453098948
-  //   },
-  //   {
-  //     date: "2025-04-16",
-  //     normalized_accuracy: 0.23,
-  //     normalized_rt: 0.7349693311852756
-  //   },
-  //   {
-  //     date: "2025-04-17",
-  //     normalized_accuracy: 0.6,
-  //     normalized_rt: 0.3590438763409098
-  //   },
-  //   {
-  //     date: "2025-04-18",
-  //     normalized_accuracy: 1,
-  //     normalized_rt: 0.2305938453098948
-  //   },
-  //   {
-  //     date: "2025-04-19",
-  //     normalized_accuracy: 0.23,
-  //     normalized_rt: 0.7349693311852756
-  //   },
-  //   {
-  //     date: "2025-04-20",
-  //     normalized_accuracy: 0.6,
-  //     normalized_rt: 0.3590438763409098
-  //   },
-  //   {
-  //     date: "2025-04-21",
-  //     normalized_accuracy: 1,
-  //     normalized_rt: 0.2305938453098948
-  //   },
-  //   {
-  //     date: "2025-04-22",
-  //     normalized_accuracy: 0.6,
-  //     normalized_rt: 0.3590438763409098
-  //   },
-  //   {
-  //     date: "2025-04-23",
-  //     normalized_accuracy: 1,
-  //     normalized_rt: 0.2305938453098948
-  //   }
-  // ]
+  const data = [
+    {
+      date: "2025-04-10",
+      normalized_accuracy: 0.23,
+      normalized_rt: 0.7349693311852756
+    },
+    {
+      date: "2025-04-11",
+      normalized_accuracy: 0.6,
+      normalized_rt: 0.3590438763409098
+    },
+    {
+      date: "2025-04-12",
+      normalized_accuracy: 1,
+      normalized_rt: 0.2305938453098948
+    },
+    {
+      date: "2025-04-13",
+      normalized_accuracy: 0.23,
+      normalized_rt: 0.7349693311852756
+    },
+    {
+      date: "2025-04-14",
+      normalized_accuracy: 0.6,
+      normalized_rt: 0.3590438763409098
+    },
+    {
+      date: "2025-04-15",
+      normalized_accuracy: 1,
+      normalized_rt: 0.2305938453098948
+    },
+    {
+      date: "2025-04-16",
+      normalized_accuracy: 0.23,
+      normalized_rt: 0.7349693311852756
+    },
+    {
+      date: "2025-04-17",
+      normalized_accuracy: 0.6,
+      normalized_rt: 0.3590438763409098
+    },
+    {
+      date: "2025-04-18",
+      normalized_accuracy: 1,
+      normalized_rt: 0.2305938453098948
+    },
+    {
+      date: "2025-04-19",
+      normalized_accuracy: 0.23,
+      normalized_rt: 0.7349693311852756
+    },
+    {
+      date: "2025-04-20",
+      normalized_accuracy: 0.6,
+      normalized_rt: 0.3590438763409098
+    },
+    {
+      date: "2025-04-21",
+      normalized_accuracy: 1,
+      normalized_rt: 0.2305938453098948
+    },
+    {
+      date: "2025-04-22",
+      normalized_accuracy: 0.6,
+      normalized_rt: 0.3590438763409098
+    },
+    {
+      date: "2025-04-23",
+      normalized_accuracy: 1,
+      normalized_rt: 0.2305938453098948
+    }
+  ]
 
   return (
     <div className="hp-container">
@@ -148,59 +149,63 @@ export default function Progress() {
       </nav>
 
       <div className="inner-box">
-        {/* <p>Accuracy</p> */}
-        <ResponsiveContainer width="100%" height="50%">
-          <AreaChart
-            data={attempts}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="date"
-              tickFormatter={(dateStr) => {
-                const date = new Date(dateStr);
-                return `${date.getUTCMonth() + 1}/${date.getUTCDate()}`;
+        <div className="chart">
+          <p>Accuracy</p>
+          <ResponsiveContainer width="100%" height="80%">
+            <AreaChart
+              data={data}//{attempts}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
               }}
-            />
-            <YAxis tickFormatter={(value) => `${(value * 100).toFixed(0)}%`} domain={[0, 1]} />
-            <Tooltip
-              formatter={(value, name) => [`${(value * 100).toFixed(2)}%`, name]}
-            />
-            <Legend />
-            <Area type="monotone" dataKey="normalized_accuracy" name="Accuracy (%)" stroke="#8884d8" fill="#8884d8" activeDot={{ r: 8 }} />
-          </AreaChart>
-        </ResponsiveContainer>
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="date"
+                tickFormatter={(dateStr) => {
+                  const date = new Date(dateStr);
+                  return `${date.getUTCMonth() + 1}/${date.getUTCDate()}`;
+                }}
+              />
+              <YAxis tickFormatter={(value) => `${(value * 100).toFixed(0)}%`} domain={[0, 1]} />
+              <Tooltip
+                formatter={(value, name) => [`${(value * 100).toFixed(2)}%`, name]}
+              />
+              <Legend />
+              <Area type="monotone" dataKey="normalized_accuracy" name="Accuracy (%)" stroke="#8884d8" fill="#8884d8" activeDot={{ r: 8 }} />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
 
-        {/* <p>Reaction Time</p> */}
-        <ResponsiveContainer width="100%" height="50%">
-          <AreaChart
-            data={attempts}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="date"
-              tickFormatter={(dateStr) => {
-                const date = new Date(dateStr);
-                return `${date.getUTCMonth() + 1}/${date.getUTCDate()}`;
+        <div className="chart">
+          <p>Reaction Time</p>
+          <ResponsiveContainer width="100%" height="80%" >
+            <AreaChart
+              data={data}//{attempts}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
               }}
-            />
-            <YAxis domain={[0, 1]} />
-            <Tooltip />
-            <Legend />
-            <Area type="monotone" dataKey="normalized_rt" name="Reaction Time" stroke="#82ca9d" fill="#82ca9d" />
-          </AreaChart>
-        </ResponsiveContainer>
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis
+                dataKey="date"
+                tickFormatter={(dateStr) => {
+                  const date = new Date(dateStr);
+                  return `${date.getUTCMonth() + 1}/${date.getUTCDate()}`;
+                }}
+              />
+              <YAxis domain={[0, 1]} />
+              <Tooltip />
+              <Legend />
+              <Area type="monotone" dataKey="normalized_rt" name="Reaction Time" stroke="#82ca9d" fill="#82ca9d" />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
       </div>
     </div>
   );
